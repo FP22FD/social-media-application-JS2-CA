6488d08f5786244498f1ead964ddbb5f977448d8
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-03-06 00:11:28 +0100

    Add function to show and hide login password

--- src/js/auth/authentication.mjs
+++ src/js/auth/authentication.mjs
@@ -7,6 +7,10 @@ const loginForm = document.querySelector("#login-form");
 registerForm?.addEventListener("submit", async (ev) => {
     ev.preventDefault();
 
+    /** @type {HTMLInputElement} */
+    const inputPws = document.querySelector("#loginPassword");
+    inputPws.setAttribute("type", "password");
+
     const form = /** @type {HTMLFormElement} */ (ev.currentTarget);
 
     const name = form.elements["username"].value;
@@ -35,11 +39,16 @@ registerForm?.addEventListener("submit", async (ev) => {
 loginForm?.addEventListener("submit", async (ev) => {
     ev.preventDefault();
 
+    /** @type {HTMLInputElement} */
+    const inputPws = document.querySelector("#loginPassword");
+    inputPws.setAttribute("type", "password");
+
     // This is called Casting or Type Casting ("force way to ignore type errors"): https://dev.to/samuel-braun/boost-your-javascript-with-jsdoc-typing-3hb3
-    const form = /** @type {HTMLFormElement} */ (ev.currentTarget);
+    const form = /** @type {HTMLFormElement} */ (ev.currentTarget);// return the form HTML element that has the event listener
 
     const email = form.elements["loginEmail"].value;
     const password = form.elements["loginPassword"].value;
+
 
     const profile = await login(email, password);
     if (profile === null) {
@@ -61,5 +70,34 @@ export function displayError(visible, text) {
         error.innerHTML = text;
     } else {
         error.style.display = "none";
+    }
+}
+
+const showPws = document.querySelector("#showPassword");
+showPws.addEventListener("click", displayLoginPassword);
+
+const hidePws = document.querySelector("#hidePassword");
+hidePws.addEventListener("click", displayLoginPassword);
+
+/** 
+ * @description Show and hide login password
+ */
+function displayLoginPassword() {
+    /** @type {HTMLInputElement} */
+    const inputPws = document.querySelector("#loginPassword");
+
+    if (inputPws.type === "password") {
+        inputPws.setAttribute("type", "text");
+
+        hidePws.classList.remove("d-flex");
+        hidePws.classList.add("d-none");
+        showPws.classList.remove("d-none");
+
+    } else {
+        inputPws.setAttribute("type", "password");
+
+        hidePws.classList.add("d-flex");
+        hidePws.classList.remove("d-none");
+        showPws.classList.add("d-none");
     }
 }

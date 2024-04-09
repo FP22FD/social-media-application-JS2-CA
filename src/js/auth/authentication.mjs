273598f567a331de3311ef11e62ebb5c7b4a29cb
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-04-09 15:11:12 +0200

    Refactoring to implement router function

--- src/js/auth/authentication.mjs
+++ src/js/auth/authentication.mjs
@@ -2,67 +2,78 @@ import { displayError } from "../shared/displayErrorMsg.mjs";
 import { login } from "./login.mjs";
 import { register } from "./register.mjs";
 
-const registerForm = document.querySelector("#register-form");
-const loginForm = document.querySelector("#login-form");
+export function init() {
 
-registerForm?.addEventListener("submit", async (ev) => {
-    ev.preventDefault();
+    addEventToLogin();
+    addEventToRegister();
 
-    /** @type {HTMLInputElement} */
-    const inputPws = document.querySelector("#loginPassword");
-    inputPws.setAttribute("type", "password");
+    const showPws = document.querySelector("#showPassword");
+    showPws.addEventListener("click", displayLoginPassword);
 
-    const form = /** @type {HTMLFormElement} */ (ev.currentTarget);
-
-    const name = form.elements["username"].value;
-    const email = form.elements["emailAddress"].value;
-    const password = form.elements["password"].value;
-    const confirmPassword = form.elements["confirmPassword"].value;
-
-    if (password !== confirmPassword) {
-        displayError(true, "#error", "The passwords have to match!");
-        return;
-    }
-    const response = await register(name, email, password);
-    if (!response) {
-        return;
-    }
-
-    const profile = await login(email, password);
-    if (profile === null) {
-        return;
-    }
-
-    window.location.href = "/profile/index.html";
-});
+    const hidePws = document.querySelector("#hidePassword");
+    hidePws.addEventListener("click", displayLoginPassword);
+}
 
 
-loginForm?.addEventListener("submit", async (ev) => {
-    ev.preventDefault();
 
-    /** @type {HTMLInputElement} */
-    const inputPws = document.querySelector("#loginPassword");
-    inputPws.setAttribute("type", "password");
+function addEventToLogin() {
+    const loginForm = document.querySelector("#login-form");
+    loginForm.addEventListener("submit", async (ev) => {
+        ev.preventDefault();
 
-    // This is called Casting or Type Casting ("force way to ignore type errors"): https://dev.to/samuel-braun/boost-your-javascript-with-jsdoc-typing-3hb3
-    const form = /** @type {HTMLFormElement} */ (ev.currentTarget);// return the form HTML element that has the event listener
+        /** @type {HTMLInputElement} */
+        const inputPws = document.querySelector("#loginPassword");
+        inputPws.setAttribute("type", "password");
 
-    const email = form.elements["loginEmail"].value;
-    const password = form.elements["loginPassword"].value;
+        // This is called Casting or Type Casting ("force way to ignore type errors"): https://dev.to/samuel-braun/boost-your-javascript-with-jsdoc-typing-3hb3
+        const form = /** @type {HTMLFormElement} */ (ev.currentTarget);// return the form HTML element that has the event listener
+
+        const email = form.elements["loginEmail"].value;
+        const password = form.elements["loginPassword"].value;
 
 
-    const profile = await login(email, password);
-    if (profile === null) {
-        return;
-    }
-    window.location.href = "/profile/index.html";
-});
+        const profile = await login(email, password);
+        if (profile === null) {
+            return;
+        }
+        window.location.href = "/profile/index.html";
+    })
+}
 
-const showPws = document.querySelector("#showPassword");
-showPws.addEventListener("click", displayLoginPassword);
+function addEventToRegister() {
+    const registerForm = document.querySelector("#register-form");
+    registerForm.addEventListener("submit", async (ev) => {
+        ev.preventDefault();
 
-const hidePws = document.querySelector("#hidePassword");
-hidePws.addEventListener("click", displayLoginPassword);
+        /** @type {HTMLInputElement} */
+        const inputPws = document.querySelector("#loginPassword");
+        inputPws.setAttribute("type", "password");
+
+        const form = /** @type {HTMLFormElement} */ (ev.currentTarget);
+
+        const name = form.elements["username"].value;
+        const email = form.elements["emailAddress"].value;
+        const password = form.elements["password"].value;
+        const confirmPassword = form.elements["confirmPassword"].value;
+
+        if (password !== confirmPassword) {
+            displayError(true, "#error", "The passwords have to match!");
+            return;
+        }
+        const response = await register(name, email, password);
+        if (!response) {
+            return;
+        }
+
+        const profile = await login(email, password);
+        if (profile === null) {
+            return;
+        }
+
+        window.location.href = "/profile/index.html";
+    })
+
+}
 
 /** 
  * @description Set the input type text<=>password.
@@ -71,6 +82,9 @@ hidePws.addEventListener("click", displayLoginPassword);
 function displayLoginPassword() {
     /** @type {HTMLInputElement} */
     const inputPws = document.querySelector("#loginPassword");
+
+    const showPws = document.querySelector("#showPassword");
+    const hidePws = document.querySelector("#hidePassword");
 
     if (inputPws.type === "password") {
         inputPws.setAttribute("type", "text");

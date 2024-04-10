ea9162c9929d91bc33720c814aa672aff5a1adf5
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-04-10 16:24:18 +0200

    Refactoring and  implement router function

---
+++ src/js/router/router.mjs
@@ -0,0 +1,51 @@
+import { init as initAuthentication } from "../auth/authentication.mjs";
+import { init as initProfile } from "../profile/profile.mjs";
+import { init as initDisplayPosts } from "../feed/feedPosts.mjs";
+import { init as initCreatePost } from "../feed/createPost.mjs";
+import { init as initSearch } from "../feed/search.mjs";
+import { init as initSinglePost } from "../feed/singlePost.mjs";
+import { init as initLogout } from "../shared/logout.mjs";
+// import { checkUserAuth } from "../shared/checkUserAuth.mjs";
+
+function router() {
+    const pathname = window.location.pathname;
+
+    console.log({ pathname });
+
+    // checkUserAuth(pathname);
+    // debugger;
+
+    switch (pathname) {
+        case "/":
+        case "/index.html":
+            console.log('login');
+
+            initAuthentication();
+            break;
+
+        case "/profile/index.html":
+            initProfile();
+            initLogout();
+            break;
+
+        case "/feed/index.html":
+            initDisplayPosts();
+            initCreatePost();
+            initSearch();
+
+            initLogout();
+            break;
+
+        case "/feed/postdetails.html":
+            initSinglePost();
+
+            initLogout();
+            break;
+
+        default:
+            // redirect to login
+            break;
+    }
+}
+
+router();
